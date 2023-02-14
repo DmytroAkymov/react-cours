0e42bfade8229515675c4231ee2383a99e0da787
Dimate <2dmitri.akimov@gmail.com>
2023-02-14 02:39:34 +0100

    First Commit

--- 06-create-react-app/src/App.js
+++ 06-create-react-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!hg
+          Learn React!!!
         </a>
       </header>
     </div>
